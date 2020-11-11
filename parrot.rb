aeb3b4fd89f2836536daf8a06129fb63b2cfbc74
Joran <jordanalireed@icloud.com>
2020-11-11 15:30:04 -0500

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,28 @@
-# Create method `parrot` that outputs a given phrase and
-# returns the phrase
+class Person
+end
+
+def parrot
+  puts "Squawk!"
+end
+
+ def parrot(name)
+   puts "Squawk!"
+ end
+
+ #       assigning a default value
+ def parrot(name = "Squawk!")
+   puts "#{name}"
+ end
+
+ pierre = Person.new
+pierre.object_id #=> 2
+
+def parrot=(parrot_name)
+  this_dogs_name = dog_name
+end
+
+def parrot
+   this_parrots_name = 'Pretty bird!'
+end
+
+parrot(pierre)
